fe3e1441b3d03e788e740a8a60ee81c0f72a7762
mlittlec <mlittlec@hotmail.com>
2020-04-28 23:05:41 +0100

    Trying Inline CSS styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,5 +5,6 @@ export default () => (
     <Layout> 
         <h1>This is our Home Page</h1>
         Hello world! <Link to="/blog/">Blog page</Link>
+        <h1 style={{fontSize: "20px", textTransform: "capitalize", color: "red"}}>Hello styling</h1>
     </Layout>
 )
