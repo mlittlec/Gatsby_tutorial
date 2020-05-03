272a34627e61904e2673e84ead6d158f2055b9d4
mlittlec <mlittlec@hotmail.com>
2020-05-03 22:32:08 +0100

    Adding the CSS files for individual pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,15 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styles from '../css/error.module.css'
+import {Link} from 'gatsby'
+import Banner from '../components/Banner'
 
 export default function error() {
-  return <Layout>If you're seeing this I've f*%ked up... (I'm the Error page)</Layout>
+  return <Layout>
+      <header className={styles.error}>
+        <Banner title="Oops I've f*%ked up...(Error page)">
+          <Link to='/' className="btn-white">Return to Home page</Link>
+        </Banner>
+      </header>
+    </Layout>
 }
