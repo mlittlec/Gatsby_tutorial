1ac461b71a1c8059344b3385420fc50a9121b2f2
mlittlec <mlittlec@hotmail.com>
2020-05-03 19:23:10 +0100

    Add a 'Simple' Hero component to the Index.js page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
 // import { FaAdobe } from "react-icons/fa"
+import SimpleHero from '../components/SimpleHero'
+import Banner from '../components/Banner'
 
 export default () => (
   <Layout>
-    <div>Hello from BackRoads</div>
+    <SimpleHero>
+      < Banner title = "Continue Exploring"
+      info = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officius." >
+        <Link to="/tours" className="btn-white">
+          Explore Tours
+        </Link>
+        
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
