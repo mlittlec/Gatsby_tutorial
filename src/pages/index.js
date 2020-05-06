46b47eac6d6a71409204570f83b567866b88515a
mlittlec <mlittlec@hotmail.com>
2020-05-06 23:36:30 +0100

    Adding the About section to the Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-// import { FaAdobe } from "react-icons/fa"
-import SimpleHero from '../components/SimpleHero'
-import Banner from '../components/Banner'
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import About from "../components/Home/About"
 
 export default () => (
   <Layout>
     <SimpleHero>
-      < Banner title = "Continue Exploring"
-      info = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officius." >
+      <Banner
+        title="continue exploring"
+        info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+      >
         <Link to="/tours" className="btn-white">
-          Explore Tours
+          explore tours
         </Link>
-        
       </Banner>
     </SimpleHero>
+    <About />
   </Layout>
 )
