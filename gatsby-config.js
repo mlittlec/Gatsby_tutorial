1420bc5f41a60f3105ca1203f6ff68b523f9ed07
mlittlec <mlittlec@hotmail.com>
2020-05-08 23:49:34 +0100

    Adding siteMetadata to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  siteMetadata:{
+    title: "BackRoads",
+    description: "Explore an awesome worldwide tour company & discover what makes us unique",
+    author: '@johndoe',
+  },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }
