701900972184787a37820ed6d73d50abefeef758
mlittlec <mlittlec@hotmail.com>
2020-05-10 23:36:15 +0100

    Adding GraphQL support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,23 @@
 module.exports = {
-  siteMetadata:{
+  siteMetadata: {
     title: "BackRoads",
-    description: "Explore an awesome worldwide tour company & discover what makes us unique",
+    description:
+      "Explore an awesome worldwide tour company & discover what makes us unique",
     author: "@johndoe",
+    data: {
+      name: "name example",
+      age: 24,
+    },
   },
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+  ],
 }
