b4b8005ea87d37d4565a450f0fcd6eaa82c1c73b
mlittlec <mlittlec@hotmail.com>
2020-05-15 23:29:11 +0100

    Setting up the Tour template

--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -1,7 +1,47 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-const Template = () => {
-  return <div>Hello from the dummy data template</div>
+const Template = ({ data }) => {
+  const {
+    name,
+    price,
+    country,
+    days,
+    description: { description },
+    images,
+    start,
+    journey,
+  } = data.tour
+
+  const [mainImage, ...tourImages] = images
+  console.log(mainImage)
+  console.log(tourImages)
+
+  return <h1>{name}</h1>
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTour(slug: { eq: $slug }) {
+      name
+      price
+      country
+      days
+      start(formatString: "dddd MMMM Do, YYYY")
+      description {
+        description
+      }
+      journey {
+        day
+        info
+      }
+      images {
+        fluid {
+          src
+        }
+      }
+    }
+  }
+`
 
 export default Template
